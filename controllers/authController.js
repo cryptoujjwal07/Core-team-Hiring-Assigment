93b639750408fb92afd692af125f1adf623d9e07
Ayushman Gupta <ayushmang06@gmail.com>
2026-07-21 16:48:22 +0530

    user model with bcrypt hashing

---
+++ controllers/authController.js
@@ -0,0 +1,31 @@
+const jwt = require("jsonwebtoken");
+const User = require("../models/User");
+const generateToken = (id)=>
+  jwt.sign({id}, process.env.JWT_SECRET,{expiresIn: "7d"});
+
+// User registration
+const registerUser = async (req,res) =>{
+  try{
+    const{ name, email, password } = req.body;
+    if(!name || !email || !password)
+      return res.status(400).json({message:"All fields are required"});
+
+    const userExists = await User.findOne({email});
+    if(userExists)
+      return res.status(400).json({message: "User already exists"});
+
+    const user = await User.create({name, email, password});
+
+    res.status(201).json({
+      message: "Registration successful",
+      user:{
+        id: user._id,
+        name: user.name,
+        email: user.email,
+      },
+    });
+  }
+  catch(err){
+    res.status(500).json({ message:err.message });
+  }
+};
