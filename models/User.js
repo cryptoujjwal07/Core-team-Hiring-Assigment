1f6b113c537cb762a7bd4f71dc1bc075642c0721
Ayushman Gupta <ayushmang06@gmail.com>
2026-07-21 16:50:33 +0530

    user model with bcrypt hashing

--- models/User.js
+++ models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 const userSchema = new mongoose.Schema({
   name:{
     type: String,
@@ -8,11 +9,24 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true,
     unique: true,
+    lowercase: true,
+    trim: true,
+    match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please enter a valid email address']
   },
   password:{
     type: String,
     required: true,
   },
 });
+//hash password before saving
+userSchema.pre("save", async function(next){
+  if(!this.isModified("password")) return next();
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+//compare b/w this and entered password during login
+userSchema.methods.matchPassword = function (password) {
+  return bcrypt.compare(password, this.password);
+};
 
 module.exports = mongoose.model("User", userSchema);
