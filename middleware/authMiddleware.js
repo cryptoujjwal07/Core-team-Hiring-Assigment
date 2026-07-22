dece5188284b58fba8327023f8306c4ce7f6dc8a
Ayushman Gupta <ayushmang06@gmail.com>
2026-07-22 15:58:15 +0530

    auth middleware & register user routes

---
+++ middleware/authMiddleware.js
@@ -0,0 +1,23 @@
+const jwt = require("jsonwebtoken");
+const User = require("../models/User");
+
+const protect = async (req, res, next) =>{
+  try{
+    const token = req.headers.authorization?.split(" ")[1];                      //using optional chaining
+
+    if(!token)
+      return res.status(401).json({message:"No token provided"});
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    req.user = await User.findById(decoded.id).select("-password");
+
+    if(!req.user)
+      return res.status(404).json({message:"User not found"});
+    next();
+  }
+  catch(err){
+    res.status(401).json({message:"Invalid token"});
+  }
+};
+
+module.exports = {protect};
